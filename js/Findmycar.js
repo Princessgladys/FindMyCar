ce76ef123d1393b3c222c97c2f19e768b758b2fe
sigon <mapping.and.co@gmail.com>
2014-03-01 17:16:27 +0100

    set max zoom to 17

--- js/Findmycar.js
+++ js/Findmycar.js
@@ -60,6 +60,7 @@ var map = L.map('map').setView([41.3905, 2.1914], 15);
 
 
 L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+	maxZoom: 17,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
 
