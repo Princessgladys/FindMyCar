a1e4e2e4e687aad961edddf1403000dbb9f2509e
sigon <mapping.and.co@gmail.com>
2014-03-01 18:47:54 +0100

    clean some old comment code

--- js/Findmycar.js
+++ js/Findmycar.js
@@ -1,21 +1,3 @@
-/*//geolocation options
-var options;
-
-if (navigator.userAgent.toLowerCase().match(/android/)) {
-	options = {
-	    maximumAge: 30000,
-	    timeout: 5000,
-	    enableHighAccuracy: false
-	};
-} else {
-	options = {
-	    maximumAge: 30000,
-	    timeout: 5000,
-	    enableHighAccuracy: true
-	};
-}*/
-
-
 //geolocation API
 var x = document.getElementById("texto");
 var userLat;
@@ -26,8 +8,8 @@ var accu;
 function getLocation()  {
   var options = null;
   if (navigator.geolocation) {
-    options={enableHighAccuracy: true, maximumAge: 15000, timeout: 30000};
-	navigator.geolocation.getCurrentPosition(showPosition,noPosition,options);
+    options={enableHighAccuracy: true, maximumAge: 30000, timeout: 30000};
+	navigator.geolocation.getCurrentPosition(showPosition,getNoPosition,options);
 
   }
   else {
@@ -43,7 +25,7 @@ function showPosition(position) {
 	accu= position.coords.accuracy;
 
 	var mapCenter = new L.LatLng(lat, lon);
-	map.setView(mapCenter, 24);
+	map.setView(mapCenter, 17);
 
 	L.marker(mapCenter).addTo(map)
 	.bindPopup("You are within " + accu + " meters from this point").openPopup();
@@ -51,10 +33,9 @@ function showPosition(position) {
 	L.circle(mapCenter, accu).addTo(map);
 }
 
-function noPosition(position) {
+function getNoPosition(position) {
 	//map.setView([41.3905, 2.1914], 15);
-	x.innerHTML = "Geolocation is not supported by this browser"
-	alert('no hemos podido localizar tu posicion')
+	x.innerHTML = "Geolocation is not supported"
 }
 var map = L.map('map').setView([41.3905, 2.1914], 15);
 
@@ -65,32 +46,13 @@ L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/
 }).addTo(map);
 
 
-/*
-function onLocationFound(e) {
-	var radius = e.accuracy / 2;
-
-	L.marker(e.latlng).addTo(map)
-		.bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-	L.circle(e.latlng, radius).addTo(map);
-}
-
-function onLocationError(e) {
-	alert(e.message);
-}
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-map.locate({setView: true, maxZoom: 16});
-*/
-map.on('moveend', function() {
+/*map.on('moveend', function() {
     console.log("map was panned!");
     console.log("zoom: " + map.getZoom());    // prints out zoom level
     console.log("center: " + map.getCenter());    // prints out map center
     //console.log(map.getBounds());    // throws error
     console.log("min zoom "+map.getMinZoom() + ' max '+ map.getMaxZoom() )
-});
+});*/
 
 
 
