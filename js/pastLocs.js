7ab5aef10212b66fbab637e71ca866dab867776e
sigon <mapping.and.co@gmail.com>
2014-03-16 13:34:39 +0100

    change zoom

--- js/pastLocs.js
+++ js/pastLocs.js
@@ -1,8 +1,8 @@
-var map = L.map('map').setView([41.3904, 2.1914], 15);
+var map = L.map('map').setView([41.3904, 2.1914], 14);
 
 //the base map:
 L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-	maxZoom: 17,
+	//maxZoom: 17,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
 
@@ -29,7 +29,7 @@ else {
 		var fecha =milliseconds.getFullYear()+'/'+mes+'/'+milliseconds.getDate()+', '+milliseconds.getHours()+':'+milliseconds.getMinutes();
 		console.log('position '+i+': '+fecha+', parked at '+ lat+', '+lon)
 		var carLatLng = new L.LatLng(lat, lon);
-		map.setView(carLatLng, 15);
+		map.setView(carLatLng, 14);
 		
 		// add marker
 		L.marker(carLatLng).addTo(map).bindPopup("parked on:</br>"+fecha+"h").openPopup();
