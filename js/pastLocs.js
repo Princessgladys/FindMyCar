5bcb17da8941961fa7c8cfc4ea2501514d77f8b7
sigon <mapping.and.co@gmail.com>
2014-03-16 18:53:03 +0100

    add heat map

--- js/pastLocs.js
+++ js/pastLocs.js
@@ -33,9 +33,34 @@ else {
 		
 		// add marker
 		L.marker(carLatLng).addTo(map).bindPopup("parked on:</br>"+fecha+"h").openPopup();
-
 	};
-
-
-
 }
+var addressPoints = [
+[41.3904, 2.1914, 0.4],
+[41.3914, 2.1924, 0.2],
+[41.3924, 2.1934, 1],
+[41.3934, 2.1994, 0.3],
+[41.3944, 2.1954, 0.65],
+[41.3954, 2.1974, 0.96]]
+
+var heat;
+function heatmap(){
+	//L.heatLayer(latlngs, options)
+	heat = L.heatLayer(addressPoints).addTo(map);
+	var options = {
+		maxZoom:17,//zoom level where the points reach maximum intensity (as intensity scales with zoom), equals maxZoom of the map by default
+		max:1,//maximum point intensity, 1.0 by default
+		radius: 25,// radius of each "point" of the heatmap, 25 by default
+        blur: 14,//amount of blur
+        /*gradient: {0.15: "rgb(0,0,255)",
+            0.35: "rgb(0,255,255)",
+            0.65: "rgb(0,255,0)",
+            0.95: "yellow",
+            1.0: "rgb(255,0,0)"}*/
+	}
+	heat.setOptions(options);
+}
+map.on("moveend", function (e) { 
+    console.log(" zoom: " +map.getZoom()+ " center: " + map.getCenter());
+    console.log(heat)
+})
