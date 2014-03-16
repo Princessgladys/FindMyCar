0cd267f89671cf4e4a965be4e5e65c3029e7a910
sigon <mapping.and.co@gmail.com>
2014-03-16 19:04:23 +0100

    add heatmap points from localhost

--- js/pastLocs.js
+++ js/pastLocs.js
@@ -10,6 +10,8 @@ L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/
 var localDBlength =localStorage.length;
 console.log('localDBlength: '+localDBlength)
 
+var arrayPoints = [];
+
 if (localDBlength == 0) {
 	alert('Sorry, No car position available!!')
 }
@@ -30,37 +32,30 @@ else {
 		console.log('position '+i+': '+fecha+', parked at '+ lat+', '+lon)
 		var carLatLng = new L.LatLng(lat, lon);
 		map.setView(carLatLng, 14);
-		
 		// add marker
 		L.marker(carLatLng).addTo(map).bindPopup("parked on:</br>"+fecha+"h").openPopup();
+		// 
+		arrayPoints.push([lat,lon]);
+		console.log(arrayPoints[i]);
 	};
 }
-var addressPoints = [
-[41.3904, 2.1914, 0.4],
-[41.3914, 2.1924, 0.2],
-[41.3924, 2.1934, 1],
-[41.3934, 2.1994, 0.3],
-[41.3944, 2.1954, 0.65],
-[41.3954, 2.1974, 0.96]]
+console.log(arrayPoints)
 
-var heat;
+
 function heatmap(){
 	//L.heatLayer(latlngs, options)
-	heat = L.heatLayer(addressPoints).addTo(map);
+	var heat = L.heatLayer(arrayPoints).addTo(map);
 	var options = {
 		maxZoom:17,//zoom level where the points reach maximum intensity (as intensity scales with zoom), equals maxZoom of the map by default
-		max:1,//maximum point intensity, 1.0 by default
+		max:1.0,//maximum point intensity, 1.0 by default
 		radius: 25,// radius of each "point" of the heatmap, 25 by default
-        blur: 14,//amount of blur
+        blur: 15,//amount of blur
         /*gradient: {0.15: "rgb(0,0,255)",
             0.35: "rgb(0,255,255)",
             0.65: "rgb(0,255,0)",
             0.95: "yellow",
             1.0: "rgb(255,0,0)"}*/
 	}
-	heat.setOptions(options);
-}
-map.on("moveend", function (e) { 
-    console.log(" zoom: " +map.getZoom()+ " center: " + map.getCenter());
-    console.log(heat)
-})
+	//heat.setOptions(options);
+};
+
