586cbc7e60cc4a224613c7515b8f4ee7dca97993
sigon <mapping.and.co@gmail.com>
2014-03-17 13:59:05 +0100

    set max zoom: 18

--- js/seeLoc.js
+++ js/seeLoc.js
@@ -2,7 +2,7 @@ var map = L.map('map').setView([41.3904, 2.1914], 15);
 
 //the base map:
 L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-	maxZoom: 14,
+	maxZoom: 18,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 }).addTo(map);
 
