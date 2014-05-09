27d760d924a5941c50a1715b6ea8736b8aae4e2d
sigon <mapping.and.co@gmail.com>
2014-05-09 10:37:59 +0200

    update seeloc.js

--- js/seeLoc.js
+++ js/seeLoc.js
@@ -69,13 +69,20 @@ function showPosition(position) {
 	mylon = position.coords.longitude;
 	accu= position.coords.accuracy;
 
-	var mapCenter = new L.LatLng(mylat, mylon);
-	map.setView(mapCenter, 16);
+	var myPosition = new L.LatLng(mylat, mylon);
+	var centerlat= +lat;
+	centerlat= centerlat+0.0005
+	map.setView([centerlat,lon], 16);
 
-	L.marker(mapCenter).addTo(map)
-	.bindPopup("You are within " + accu + " meters from this point").openPopup();
+	var distance2car= getDistanceFromLatLonInKm(mylat, mylon, carlat, carlon);
 
-	L.circle(mapCenter, accu).addTo(map);
+	L.marker(myPosition).addTo(map)
+	.bindPopup("You are within " + distance2car.round(2) + " km from your car").openPopup();
+
+	L.circle(myPosition, accu).addTo(map);
+	routeButton.removeClass( "hide" );
+	getLocationButton.addClass("hide");
+	texto.addClass("hide");
 }
 
 function getNoPosition(position) {
@@ -97,5 +104,21 @@ function getRoute() {
 	}).addTo(map);
 };
 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
